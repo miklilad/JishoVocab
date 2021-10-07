81ffef4d102ec09d5ae1872e641350c014ccdaee
miklilad <ladislav@miklik.eu>
2021-10-07 20:42:41 +0200

    0.9.1

--- dist/main.user.js
+++ dist/main.user.js
@@ -6,6 +6,7 @@
 // @author       miklilad
 // @match        *://jisho.org/*
 // @icon         https://www.google.com/s2/favicons?domain=jisho.org
+// @require      https://github.com/miklilad/JishoVocab/blob/main/dist/AnkiExporter.js
 // @supportURL   https://github.com/miklilad/JishoVocab/issues
 // @grant        GM_setValue
 // @grant        GM_listValues
